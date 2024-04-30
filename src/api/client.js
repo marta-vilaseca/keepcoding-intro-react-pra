853670581f40d8ea55c3de71c7da56e66a779be4
Marta <vilaseca.marta@gmail.com>
2024-04-30 18:52:30 +0200

    Solucionado error de importación en las variables de entorno

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 client.interceptors.response.use(
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
   return {
-    define: {
-      "process.env.REACT_APP_API_BASE_URL": JSON.stringify(env.REACT_APP_API_BASE_URL),
-    },
     plugins: [svgr(), react()],
   };
 });
